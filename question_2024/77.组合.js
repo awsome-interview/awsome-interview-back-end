230286f545ab0366de0ed9329696648469363787
qiufeihong2018 <15058301288@163.com>
2024-05-01 19:10:44 +0800

    docs: update awsome-knowledge-back-end/question_2024/77.组合.js

--- question_2024/77.组合.js
+++ question_2024/77.组合.js
@@ -11,6 +11,30 @@
  * @return {number[][]}
  */
 var combine = function (n, k) {
+  // 一：非剪枝
+
+  // // 回溯法
+  // let result = [],
+  //   path = [];
+  // let backtracking = (_n, _k, startIndex) => {
+  //   // 终止条件
+  //   if (path.length === _k) {
+  //     result.push(path.slice());
+  //     return;
+  //   }
+  //   // 循环本层集合元素
+  //   for (let i = startIndex; i <= _n; i++) {
+  //     path.push(i);
+  //     //   递归
+  //     backtracking(_n, _k, i + 1);
+  //     //   回溯操作
+  //     path.pop();
+  //   }
+  // };
+  // backtracking(n, k, 1);
+  // return result;
+
+  // 二：剪枝
   // 回溯法
   let result = [],
     path = [];
@@ -21,7 +45,7 @@ var combine = function (n, k) {
       return;
     }
     // 循环本层集合元素
-    for (let i = startIndex; i <= _n; i++) {
+    for (let i = startIndex; i <= _n - (_k - path.length) + 1; i++) {
       path.push(i);
       //   递归
       backtracking(_n, _k, i + 1);
